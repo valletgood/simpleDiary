32d9201704d4e1c3fd7bdbafe809d90d6a5f9e59
valletgood <valletgood@naver.com>
2022-11-05 15:41:46 +0900

    20221105

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
+
+// https://jsonplaceholder.typicode.com/comments
 
 function App() {
 
   const [data, setData] = useState([]);
 
   const dataId = useRef(0);
+
+  const getData = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/comments').then((res) => res.json())
+
+    const initData = res.slice(0, 20).map((it) => {
+      return {
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        created_date: new Date().getTime(),
+        id: dataId.current++,
+      }
+    })
+
+    setData(initData);
+  };
+
+  useEffect(() => {
+    getData();
+  }, [])
 
   const onCreate = (author, content, emotion) => {
     const created_date = new Date().getTime();
